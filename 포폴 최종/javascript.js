ea2b6cc83c83c9386270a300bfc23cbf7eb87cb0
Leeseokgyu <snowman021828@gmail.com>
2024-10-07 15:54:24 +0900

    Add files via upload

---
+++ 포폴 최종/javascript.js
@@ -0,0 +1,21 @@
+let slide = new Swiper('#sn .swiper',{
+    direction:'horizontal',
+    autoplay:{delay:6000,},
+    loop:true,
+    slidesPerView:3,
+    spaceBetween:15,
+    navigation:{
+        nextEl:'#sn .swiper-button-next',
+        prevEl:'#sn .swiper-button-prev'
+    }
+})
+let slide2 = new Swiper('#bn .swiper',{
+    direction:'horizontal',
+    autoplay:{delay:3000,},
+    loop:true,
+    slidesPerView:2,
+    navigation:{
+        nextEl:'#bn .swiper-button-next',
+        prevEl:'#bn .swiper-button-prev'
+    }
+})
